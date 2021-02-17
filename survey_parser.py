0b98fe14029b72caca6e1024a85a19ae460e6c51
xperthunter <kjfraga@ucdavis.edu>
2021-02-16 16:55:03 -0800

    plotting ad table making improved

--- survey_parser.py
+++ survey_parser.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 
+import argparse
 import json
+import os
+import re
 import sys
 import csv
 import matplotlib.pyplot as plt
 
+parser = argparse.ArgumentParser(description='DEIC Survey Parser and Plotter')
+parser.add_argument('--csv','-c', required=True, type=str,
+	metavar='<str>', help='survey csv')
+parser.add_argument('--dir', '-d', required=True, type=str, 
+	help='directory to store images')
+
+arg = parser.parse_args()
+
 welcome_ranking = dict()
 
-freeresponse    = {'OtherFR':[], 'Actions':[], 'FreeRep Com':[]}
+freeresponse    = {'OtherFR':[], 'Actions':[], 'FreeRep Com':[], 'O-Q1':[],
+					'O-Q2':[],'Transportation':[], 'P-M5':[], 'P-M4':[],
+					'P-M2':[],'P-B':[], 'P-B4':[], 'P-B2':[], 'P-D5':[],
+					'P-D4':[],'P-D2':[], 'P-B5':[]}
 surveyquestions = dict()
 surveydata      = dict()
 frontmatter     = ['StartDate', 'EndDate', 'Status', 'Progress', 
@@ -16,10 +30,10 @@ frontmatter     = ['StartDate', 'EndDate', 'Status', 'Progress',
 				   'Race', 'LGBTQIA', 'Identities', 'Citizen', 'Internat',
 				   'Immig', 'Year']
 quals = {'race' : None, 'lgbt' : None, 'idmg' : None, 
-		 'cit' : None, 'intn' : None, 'immg' : None, 
-		 'idcg' : None, 'cicg' : None}
+		 'citz' : None, 'intn' : None, 'immg' : None, 
+		 'Identity Group' : None, 'Citizen Group' : None}
 
-with open(sys.argv[1], mode='r') as csv_file:
+with open(arg.csv, mode='r') as csv_file:
 	csv_reader = csv.DictReader(csv_file)
 	
 	for row in csv_reader:
@@ -42,12 +56,23 @@ with open(sys.argv[1], mode='r') as csv_file:
 			quals['immg'] = row['Immig']
 			
 			identities = quals['race'] == 'Yes' or quals['lgbt'] == 'Yes' or quals['idmg'] == 'Yes'
-			citizen = quals['citz'] == 'Yes' or quals['intn'] == 'Yes' or quals['immg'] == 'Yes'
-			quals['idcg'] = identities
-			quals['cicg'] = citizen
+			citizen = quals['citz'] == 'No' or quals['intn'] == 'Yes' or quals['immg'] == 'Yes'
+			margin  = quals['race'] == 'Yes' or quals['lgbt'] == 'Yes' or quals['idmg'] == 'Yes' or quals['citz'] == 'No' or quals['intn'] == 'Yes' or quals['immg'] == 'Yes'
+			quals['Identity Group'] = identities
+			quals['Citizen Group'] = citizen
+			quals['Marginalized Group'] = margin
 			
+			for q, k in quals.items():
+				if 'vars' not in surveydata: surveydata['vars'] = dict()
+				if q not in surveydata['vars']:
+					surveydata['vars'][q] = dict()
+				if k not in surveydata['vars'][q]:
+					surveydata['vars'][q][k] = 0
+				
+				surveydata['vars'][q][k] += 1
 			
 			for q in surveydata:
+				if q == 'vars': continue
 				if len(row[q]) == 0: row[q] = 'NoResponse'
 				if row[q] not in surveydata[q]['total']:
 					surveydata[q]['total'][row[q]] = 0
@@ -62,20 +87,100 @@ with open(sys.argv[1], mode='r') as csv_file:
 					
 					surveydata[q][c][quals[c]][row[q]] += 1
 				
+with open('survey_data.json', 'w') as fp:
+	json.dump(surveydata, fp)
 
-print(json.dumps(surveydata,indent=4))
-		
+with open('survey_questions.json', 'w') as f:
+	json.dump(surveyquestions, f)
+
+#sys.exit()
+labels = ['Strongly Agree', 'Somewhat Agree', 'Neither Agree nor Disagree',
+		  'Somewhat Disagree', 'Strongly Disagree', 'NoResponse', 'Prefer not to answer']
+#answ = [k.replace(' ','\n') for k in labels]
+colors = {'Strongly Agree':'lightseagreen', 'Somewhat Agree':'turquoise', 
+		'Neither Agree nor Disagree':'thistle', 'Somewhat Disagree':'palevioletred',
+		'NoResponse':'darkgray', 'Strongly Disagree':'crimson',
+		'Prefer not to answer':'grey'}
+pres = []
+skip = False
 for q in surveydata:
+	if q == 'vars': continue
+	qname = q
+	qname = qname.replace(" ", "_")
+	qname = qname.replace("/", "_")
+	print(qname)
+	head = surveyquestions[q]
+	head = re.sub('.+: - ', '', head)
 	for level in surveydata[q]:
 		if level == 'total':
-			vals = surveydata[q]['total'].values()
-			answ = surveydata[q]['total'].keys()
-			plt.pie(vals, labels=answ, autopct='%1.2f')
-			plt.title(surveyquestions[q])
-			plt.show()
-			sys.exit()
-	
-
+			pres = [k for k in surveydata[q]['total'].keys() if k in labels]
+			if len(pres) > 0:
+				answ = [k for k in labels if k in pres]
+				vals = [surveydata[q]['total'][k] for k in answ]
+				cols = [c for k,c in colors.items() if k in answ]
+				answ = [k.replace(' ', '\n') for k in answ]
+			else:
+				answ = [k.replace(' ', '\n') for k in surveydata[q]['total'].keys()]
+				vals = [v for v in surveydata[q]['total'].values()]
+			
+			 
+			fig, ax = plt.subplots(figsize=(5.25,5.25))
+			fig.suptitle(head,wrap=True,fontsize=12)
+			if len(pres) > 0:
+				print(answ)
+				print(pres)
+				ax.pie(vals, labels=answ, autopct='%1.1f', 
+					textprops={'fontsize':6}, colors=cols)
+			else:
+				ax.pie(vals, labels=answ, autopct='%1.1f',
+					textprops={'fontsize':6})
+			
+			fig.tight_layout()
+			filename = os.path.join(arg.dir,qname+'_total.png')
+			fig.savefig(filename,dpi=200)
+			plt.close()
+		else:
+			data_keys = []
+			for k in surveydata[q][level].keys():
+				if k is None: continue
+				if type(k) is bool:
+					data_keys.append(k)
+					continue
+				if len(k) == 0: continue
+				data_keys.append(k)
+			
+			print(data_keys)
+			if 'Yes' in data_keys: dkeys = ['Yes', 'No']
+			elif type(data_keys[0]) == bool: dkeys = [True, False]
+			
+			fig, axs = plt.subplots(ncols=len(data_keys),nrows=1,figsize=(10.5,5.25))
+			fig.suptitle(head,wrap=True,fontsize=12)
+			for i, dk in enumerate(dkeys):
+				pres = [k for k in surveydata[q][level][dk].keys() if k in labels]
+				if len(pres) > 0:
+					answ = [k for k in labels if k in pres]
+					vals = [surveydata[q][level][dk][k] for k in answ]
+					cols = [c for k,c in colors.items() if k in answ]
+					answ =[k.replace(' ', '\n') for k in answ]
+				else:
+					answ = [k.replace(' ', '\n') for k in surveydata[q][level][dk].keys()]
+					vals = [v for v in surveydata[q][level][dk].values()]			
+				
+				if len(pres) > 0:
+					axs[i].pie(vals, labels=answ, autopct='%1.1f', 
+						textprops={'fontsize':6}, colors=cols)
+				else:
+					axs[i].pie(vals, labels=answ, autopct='%1.1f',
+						textprops={'fontsize':6})
+				
+				axs[i].title.set_text(f'{level} - {dk}')
+			
+			filename = os.path.join(arg.dir,qname+'_'+level+'.png')
+			fig.savefig(filename,dpi=200)
+			plt.close()
+"""
+Sorting the agreement level
+"""
 """
 for every question in surveydata:
 	vals = surveydata[q]['total'].values()
---
+++ tables.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3 
+
+import csv
+import json
+import re
+import sys
+
+from pytablewriter import MarkdownTableWriter
+
+labels = ['Strongly Agree', 'Somewhat Agree', 'Neither Agree nor Disagree',
+		  'Somewhat Disagree', 'Strongly Disagree', 'NoResponse', 'Prefer not to answer']
+names = {'race': 'Race', 'lgbt': 'LGBTQIA', 'idmg': 'Marginalized Identity',
+		 'citz': 'Citizen', 'intn': 'International', 'immg': 'Immigrant'}
+
+with open(sys.argv[1], 'r') as fp:
+	data = json.load(fp)
+	
+with open(sys.argv[2], 'r') as fp:
+	questions = json.load(fp)
+
+vvm = []
+for q in sorted(data['vars'].keys()):
+	if q in names:
+		qn = names[q]
+	else: qn = q
+	if 'Yes' in data['vars'][q]:
+		vvm.append([qn,data['vars'][q]['Yes'],data['vars'][q]['No']])
+	else:
+		vvm.append([qn,data['vars'][q]['true'],data['vars'][q]['false']])
+
+writer = MarkdownTableWriter(
+	table_name="Identity Variables",
+	headers=["Variable", "Yes Count", "No Count"],
+	value_matrix=vvm,
+	margin=1
+)
+
+writer.write_table()
+
+print()
+for q in data:
+	if q == 'vars': continue
+	title=questions[q]
+	title = re.sub('.+: - ', '', title)
+	vvm = []
+	opts = sorted(data[q]['total'].keys())
+	row = ['Total']
+	d   = [data[q]['total'][k] for k in opts]
+	row.extend(d)
+	vvm.append(row)
+	for level in sorted(data[q].keys()):
+		if level == 'total': continue
+		
+		for ans in sorted(data[q][level].keys()):
+			if level not in names: name = level+' - '+ans
+			else:                  name = names[level]+' - '+ans
+			row = [name]
+			d = []
+			for opt in opts:
+				if opt not in data[q][level][ans]:
+					d.append(0)
+				else:
+					d.append(data[q][level][ans][opt])
+			row.extend(d)
+			vvm.append(row)
+	h = ["Variable"]
+	h.extend(opts)
+	writer = MarkdownTableWriter(
+		table_name=title,
+		headers=h,
+		value_matrix=vvm,
+		margin=1
+	)
+	writer.write_table()
+	print()
